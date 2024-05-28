47cc3ff52eaa288bdf8150713bc1a9f16f961c1f
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2024-05-28 18:15:28 +0200

    chore: prepare for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontendmentor-blog-preview-card/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
